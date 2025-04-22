c5a9323608052fc53e510c68235d4f5919634ce4
redsteadz <hamees.ehsan@gmail.com>
2025-04-22 22:30:16 +0500

    optimization: a little bit more optimized code
    Needs persistent caching for better loading times
    Reduction in the nodejs modules payloads

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,11 +5,33 @@ import { and, asc, eq } from "drizzle-orm";
 import { usersTable, Completion, Habit } from "@/server/db/schema";
 import { auth } from "../api/auth/[...nextauth]/route";
 import { addDays, format, formatDate, subDays } from "date-fns";
+import { Suspense, cache } from "react";
 
 type HabitWithCompletions = Habit & {
   completions: Completion[];
   todayStatus?: "done" | "skipped" | null;
 };
+
+const getUserHabits = cache(async (id: number) => {
+  const habits: HabitWithCompletions[] = await db.query.habitsTable.findMany({
+    where: eq(habitsTable.userId, id),
+    with: {
+      completions: {
+        orderBy: (completions, { asc }) => [asc(completions.date)],
+      },
+    },
+  });
+
+  return habits;
+});
+
+const getUser = cache(async (email: string) => {
+  const user = await db.query.usersTable.findFirst({
+    where: eq(usersTable.email, email),
+    columns: { id: true },
+  });
+  return user;
+});
 
 export default async function DashboardPage() {
   // Read the Habits associated with the user
@@ -18,63 +40,56 @@ export default async function DashboardPage() {
   // console.log("email:", email);
   // from the email get the userID
 
-  const userId = await db
-    .select()
-    .from(usersTable)
-    .where(eq(usersTable.email, email?.toString()));
+  const user = await getUser(email);
+  if (!user) throw new Error("User not found");
   // // from the user ID, retrieve all the Habits
   // console.log("userId: ", userId);
-  const habits: HabitWithCompletions[] = await db.query.habitsTable.findMany({
-    where: eq(habitsTable.userId, userId[0]?.id),
-    with: {
-      completions: {
-        orderBy: (completions, { asc }) => [asc(completions.date)],
-      },
-    },
-  });
+  const habits: HabitWithCompletions[] = await getUserHabits(user.id!);
 
   const today = new Date();
   const yesterday = subDays(today, 1);
-  const fixedHabits: HabitWithCompletions[] = await Promise.all(
-    habits.map(async (habit) => {
-      // update the streak according to if the previous day is completed
-      const prevCompletion = await db.query.completionsTable.findFirst({
-        where: and(
-          eq(completionsTable.habitId, habit.id),
-          eq(completionsTable.date, yesterday.toISOString()),
+  const fixedHabits: HabitWithCompletions[] = habits.map((habit) => {
+    // update the streak according to if the previous day is completed
+    const prevCompletion = habit.completions.find((c) =>
+      format(new Date(c.date), "yyyy-MM-dd").includes(
+        format(yesterday, "yyyy-MM-dd"),
+      ),
+    );
+    const todayCompletion = habit.completions.find((c) =>
+      format(new Date(c.date), "yyyy-MM-dd").includes(
+        format(today, "yyyy-MM-dd"),
+      ),
+    );
+
+    if (!prevCompletion?.completed) {
+      // console.log(todayCompletion);
+      habit.streak = todayCompletion?.completed ? habit.streak : 0;
+    }
+
+    let newCompletions: Completion[] = [];
+    // for each hobby, return the completion of the last 7 days
+    // from last saturday to next friday
+    const lastSat = subDays(today, today.getDay() % 7);
+
+    for (let i = 0; i < 7; i++) {
+      const date = addDays(lastSat, i);
+      const completion = habit.completions.find((c) =>
+        format(new Date(c.date), "yyyy-MM-dd").includes(
+          format(date, "yyyy-MM-dd"),
         ),
+      );
+      newCompletions.push({
+        id: completion ? completion.id : 0,
+        habitId: habit.id,
+        date: date.toISOString(),
+        completed: completion?.completed ? true : false,
       });
-      if (prevCompletion?.completed ?? false) {
-        habit.streak = 0;
-      }
-
-      let newCompletions: Completion[] = [];
-      // for each hobby, return the completion of the last 7 days
-      // from last saturday to next friday
-      const lastSat = subDays(today, today.getDay() % 7);
-      // set todayStatus to the completion of today
-
-      for (let i = 0; i < 7; i++) {
-        const date = addDays(lastSat, i);
-        const completion = habit.completions.find((c) =>
-          format(new Date(c.date), "yyyy-MM-dd").includes(
-            format(date, "yyyy-MM-dd"),
-          ),
-        );
-        newCompletions.push({
-          id: completion ? completion.id : 0,
-          habitId: habit.id,
-          date: date.toISOString(),
-          completed: completion?.completed ? true : false,
-        });
-      }
-      return {
-        ...habit,
-        completions: newCompletions,
-      };
-    }),
-  );
-
+    }
+    return {
+      ...habit,
+      completions: newCompletions,
+    };
+  });
   // console.log("Habits: ", habits);
   return (
     <main className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 py-8">
